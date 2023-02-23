ec33b2f3d81d3cbca17d0917d1b821ffe53cfeda
dnd030 <108931250+dnd030@users.noreply.github.com>
2023-02-23 22:20:09 +0900

    2.23
    2.23

--- js/greetings.js
+++ js/greetings.js
@@ -6,8 +6,8 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault(HIDDEN_CLASSNAME);
-    loginForm.classList.add("hidden");
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
@@ -18,11 +18,7 @@ function paintGreetings(username){
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
-
-
-const saveUsername = localStorage.getItem(USERNAME_KEY);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
--- js/quotes.js
+++ js/quotes.js
@@ -21,30 +21,32 @@ const quotes = [
     },
     {
         quote: 'The fastest way to change yourself is to hang out with people who are already the way you want to be',
-        author: 'REid Hoffman'
+        author: 'REid Hoffman',
         },
         {
         quote: 'Money is like gasoline during a road trip. You do not want to run out of gas on your trip, but you are not doing a tour of gas stations',
-        author: 'Tim O Reilly'
+        author: 'Tim O Reilly',
         },
         {
         quote: 'Some people dream of success, while other people get up every morning and make it happen',
-        author: 'Wayne Huizenga'
+        author: 'Wayne Huizenga',
         },
         {
         quote: 'The only thing worse than starting something and falling.. is not starting something',
-        author: 'SEth Godin'
+        author: 'SEth Godin',
         },
         {
         quote: 'If you really want to do something, you will find a way. If you do not, you will find an excuse.',
-        author: 'Jim Rohn'
+        author: 'Jim Rohn',
         },
-    
 ];
 
-const quote = document.querySelector("#quote sapn:first-child");
-const author = document.querySelector("#quote sapn:nth-child(2)");
-const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+const quote = document.querySelector("#sayings span:first-child");
+const author = document.querySelector("#sayings span:last-child");
 
-quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+const toDaysQuote=quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = toDaysQuote.quote;
+author.innerText = toDaysQuote.author;
+
+console.log(toDaysQuote);
